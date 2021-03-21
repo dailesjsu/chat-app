714d5a5877be5747ce7b9f3740e41203727ac1f1
Dai Le <dai.n.le@sjsu.edu>
2021-03-21 00:14:42 -0700

    finish signup and login with formik

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,3 +1,50 @@
+import { Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FormField } from 'components';
+import { defaultValues, validationSchema } from './formikConfig';
+
 export const Signup = () => {
-    return <> I am the Sign Up</>
-}
+  const history = useHistory();
+  const [serverError, setServerError] = useState('');
+
+  const signup = ({ email, userName, password }, { setSubmitting }) => console.log('Signing Up: ', email, userName, password);
+
+  return (
+    <div className="auth-form">
+      <h1>Signup</h1>
+      <Formik
+        onSubmit={signup}
+        validateOnMount={true}
+        initialValues={defaultValues}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, isSubmitting }) => (
+          <Form>
+            <FormField name="userName" label="User Name" />
+            <FormField name="email" label="Email" type="email" />
+            <FormField name="password" label="Password" type="password" />
+            <FormField
+              type="password"
+              name="verifyPassword"
+              label="Verify Password"
+            />
+
+            <div className="auth-link-container">
+              Already have an account?{' '}
+              <span className="auth-link" onClick={() => history.push('login')}> {/* This is to link back to login page if account is already existed */}
+                Log In!
+              </span>
+            </div>
+
+            <button disabled={isSubmitting || !isValid} type="submit"> {/* Sign up is clickable if all info was filled */}
+              Sign Up
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      {!!serverError && <div className="error">{serverError}</div>} {/* error will happen if trying sign up with existing account */}
+    </div>
+  );
+};
